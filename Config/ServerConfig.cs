d4d5febe46177760b355e7ddf968ffd6b3aff340
hio <287581087+hio-2347@users.noreply.github.com>
2026-09-09 18:03:59 -0400

    Add ServerConfig class with initial configuration

--- Config/ServerConfig.cs
+++ Config/ServerConfig.cs
@@ -1,1 +1,17 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace RecRoom.Config
+{
+    public class ServerConfig
+    {
+        public static object Bracket => new List<object>(); 
+        public static string BaseURL = "http://localhost:2059";
+        public static int GameVersion = 20200320;
+        public static bool isDormPrivate = true; // doesn't work.
+    }
+}
