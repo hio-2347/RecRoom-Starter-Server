a14cdb26773d09a194ed15ec81c38a88e805fe9a
hio <287581087+hio-2347@users.noreply.github.com>
2026-09-08 22:07:49 -0400

    Add comment for printing form data in JSON format
    Added a comment explaining how to print form data as JSON.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
+namespace RecRoom
+{
+    public class Program
+    {
+        /* 
+         * Console.WriteLine(JsonSerializer.Serialize(ctx.Request.Form.ToDictionary(k => k.Key, v => v.Value.ToString()), new JsonSerializerOptions { WriteIndented = true }));
+         * above to print form in json
+        */
 
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var webApplication = builder.Build();
+
+
+            app.Init();
+
+            Signatures.Init();
+
+            app.Run("http:localhost:2059");
+        }
+    }
+}
